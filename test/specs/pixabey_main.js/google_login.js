94146323a22cf856587ad325fbbfbcc56cec1983
Ruslan Moiseyenko <111799908+Yulia097@users.noreply.github.com>
2023-08-16 12:00:00 +0300

    changed the photo_lke

--- test/specs/pixabey_main.js/google_login.js
+++ test/specs/pixabey_main.js/google_login.js
@@ -1,6 +1,6 @@
 const { remote } = require('webdriverio');
 
-describe('Login with Google Test', () => {
+describe('Login Test', () => {
   let browser;
 
   before(async () => {
@@ -15,25 +15,50 @@ describe('Login with Google Test', () => {
     await browser.deleteSession();
   });
 
-  it('should log in with Google successfully on Pixabay', async () => {
-    await browser.url('https://pixabay.com/');
+  it('should log in with specified username and password', async () => {
+    await browser.url('https://pixabay.com');
+
+    console.log('Clicking on the login button');
+    const loginButton = await browser.$('//span[normalize-space()="Log in"]');
+    await loginButton.click();
 
     await browser.waitUntil(
       async () => {
-        const googleLoginButton = await browser.$(
-          'button[class="button--Xk14x google--xxLcM"]'
-        );
-        await googleLoginButton.click();
-
-        const loggedInElement = await browser.$(
-          '//ul[@class="pure-menu-children"]//a[contains(text(),"My profile")]'
-        );
-        expect(loggedInElement).toBeExisting();
+        console.log('Entering username and password');
+        const usernameInput = await browser.$('//input[@name="login_user"]');
+        const passwordInput = await browser.$('//input[@name="login_pass"]');
+        await usernameInput.setValue('testforqaq');
+        await passwordInput.setValue('test12345');
+        return true;
       },
       {
         timeout: 5000,
-        timeoutMsg: 'Timeout Error'
+        timeoutMsg: 'Timeout while entering username and password'
       }
     );
+
+    console.log('Clicking on the submit button');
+    const submitButton = await browser.$(
+      '//button[@class="loginButton--cVPDu e2e-auth-login-submit-button base--o-Oap primary--uRlHk"]'
+    );
+    await submitButton.click();
+
+    await browser.waitUntil(
+      async () => {
+        console.log('Waiting for user to log in');
+        const doesExist = await browser
+          .$(
+            'button[class="loginButton--uIEF2 buttonBase--r4opq tertiaryButton--+4ehJ base--jzyee dark--jRuwC"] span[class="label--Ngqjq"]'
+          )
+          .isExisting();
+        return doesExist;
+      },
+      {
+        timeout: 30000,
+        timeoutMsg: 'Timeout waiting for user to log in'
+      }
+    );
+
+    console.log('User logged in successfully');
   });
 });
--- test/specs/pixabey_main.js/photo_like.js
+++ test/specs/pixabey_main.js/photo_like.js
@@ -1,7 +1,8 @@
 const { remote } = require('webdriverio');
 
-describe('Login Test', () => {
+describe('Photo Like Test', () => {
   let browser;
+  let initialLikesCount;
 
   before(async () => {
     browser = await remote({
@@ -15,30 +16,107 @@ describe('Login Test', () => {
     await browser.deleteSession();
   });
 
-  it('should log in with specified username and password', async () => {
+  it('should log in and like a photo with specified username and password', async () => {
     await browser.url('https://pixabay.com');
 
-    console.log('Clicking on the login button...');
     const loginButton = await browser.$('//span[normalize-space()="Log in"]');
     await loginButton.click();
 
-    console.log('Entering username and password...');
-    const usernameInput = await browser.$('//input[@name="login_user"]');
-    const passwordInput = await browser.$('//input[@name="login_pass"]');
-    await usernameInput.setValue('testforqaq');
-    await passwordInput.setValue('test12345');
+    await browser.waitUntil(
+      async () => {
+        const usernameInput = await browser.$('//input[@name="login_user"]');
+        const passwordInput = await browser.$('//input[@name="login_pass"]');
+        await usernameInput.setValue('testforqaq');
+        await passwordInput.setValue('test12345');
+        return true;
+      },
+      {
+        timeout: 10000,
+        timeoutMsg: 'Timeout while entering username and password'
+      }
+    );
 
-    console.log('Clicking on the submit button...');
     const submitButton = await browser.$(
       '//button[@class="loginButton--cVPDu e2e-auth-login-submit-button base--o-Oap primary--uRlHk"]'
     );
     await submitButton.click();
 
-    console.log('Waiting for user to log in...');
-    const loggedInElement = await browser.$('//img[@alt="u_9sa10i8va1"]');
-    await browser.waitUntil(async () => await loggedInElement.isExisting(), {
-      timeout: 30000,
-      timeoutMsg: 'User did not log in within 20 seconds'
-    });
+    await browser.waitUntil(
+      async () => {
+        const doesExist = await browser
+          .$(
+            'button[class="loginButton--uIEF2 buttonBase--r4opq tertiaryButton--+4ehJ base--jzyee dark--jRuwC"] span[class="label--Ngqjq"]'
+          )
+          .isExisting();
+        return doesExist;
+      },
+      {
+        timeout: 60000,
+        timeoutMsg: 'Timeout waiting for user to log in'
+      }
+    );
+
+    console.log('User logged in successfully');
+
+    // Navigate to the photo page and perform the like action
+    await browser.url('https://pixabay.com');
+    const photoLocator = await browser.$(
+      'img[title="Download free HD stock image of Bird Falcon"]'
+    );
+    await photoLocator.click();
+
+    await browser.waitUntil(
+      async () => {
+        const currentUrl = await browser.getUrl();
+        return (
+          currentUrl ===
+          'https://pixabay.com/photos/solar-wind-borealis-northern-lights-7917659/'
+        );
+      },
+      {
+        timeout: 10000,
+        timeoutMsg: 'Timeout Error'
+      }
+    );
+
+    const likeButton = await browser.$(
+      '//span[@class="icon--L+lBh heart-outline--mC63s icon--hXcZl"]'
+    );
+    await likeButton.click();
+
+    initialLikesCount = parseInt(
+      await browser
+        .$('//span[@class="icon--L+lBh heart-outline--mC63s icon--hXcZl"]')
+        .getText(),
+      10
+    );
+
+    await browser.waitUntil(
+      async () => {
+        const updatedLikesCountElement = await browser.$(
+          '//button[@class="squareIconAndTextButton--LErXp light--uBcBI base--jzyee light--uBcBI active--nIoT4"]'
+        );
+        const updatedLikesCount = parseInt(
+          await updatedLikesCountElement.getText(),
+          10
+        );
+        return updatedLikesCount > initialLikesCount;
+      },
+      {
+        timeout: 80000,
+        timeoutMsg: 'Like action did not complete within 80 seconds'
+      }
+    );
+
+    const likedElement = await browser.$(
+      '//span[@class="icon--L+lBh heart--8Abpk icon--hXcZl"]'
+    );
+    expect(likedElement).toBeExisting();
+    expect(parseInt(await likedElement.getText())).toBe(initialLikesCount + 1);
   });
 });
+
+//00ab6b - color of the btn
+// - стан кнопки
+// switch between pages
+// share photo with facebook
